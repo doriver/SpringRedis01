c30881f21befa5a616b11f8dde3a25da35816793
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2024-06-25 12:46:51 +0900

    SpringBoot에서 Redis로 Cache기능 구현 시작함

--- src/main/java/com/example/demo/config/RedisConfig.java
+++ src/main/java/com/example/demo/config/RedisConfig.java
@@ -1,15 +1,22 @@
 package com.example.demo.config;
 
+import java.time.Duration;
+
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableCaching
 public class RedisConfig {
-
+/*
+ * 
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
 		return new LettuceConnectionFactory();
@@ -23,5 +30,16 @@ public class RedisConfig {
 		template.setValueSerializer(new StringRedisSerializer());
 		return template;
 	}
-	
+*/	
+	@Bean
+	public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+		RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig() //
+				.prefixCacheNameWith(this.getClass().getPackageName() + ".") //
+				.entryTtl(Duration.ofHours(1)) //
+				.disableCachingNullValues();
+
+		return RedisCacheManager.builder(connectionFactory) //
+				.cacheDefaults(config) //
+				.build();
+	}
 }
